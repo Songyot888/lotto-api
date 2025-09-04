0ad41148e8328654fb92e459d93a7e9821e9df0c
Songyot888 <dog1235you@gmail.com>
2025-09-04 21:58:11 +0700

    push

--- Program.cs
+++ Program.cs
@@ -23,13 +23,8 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 app.MapControllers();
