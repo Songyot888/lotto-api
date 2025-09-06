0c20177beb1e07956dfbb743e2eb3a7ff8da56d3
Eakrin <66011212070@msu.ac.th>
2025-09-06 15:59:40 +0700

    leo

---
+++ controllers/LottoController.cs
@@ -0,0 +1,19 @@
+using lotto_api.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_lotto.controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LottoController : Controller
+    {
+        private readonly LotteryDbContext _context;
+        public LottoController(LotteryDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
