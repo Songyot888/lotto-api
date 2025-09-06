6a61e65286c533b3b146a9c1560d3fb3c4d62f0f
Songyot888 <dog1235you@gmail.com>
2025-09-06 16:16:31 +0700

    ฟกก

--- obj/Debug/net9.0/lotto_api.AssemblyInfo.cs
+++ obj/Debug/net9.0/lotto_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9b5bf02869c16513eeea216303860fa445c9b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f869038566b3bdfceb808c82a8801c94fa7284a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
