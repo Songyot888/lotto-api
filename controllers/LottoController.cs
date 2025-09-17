af3eb8c17cd759eae08d43fe1a161d8a74fa43dd
Songyot888 <dog1235you@gmail.com>
2025-09-17 23:46:07 +0700

    push

--- controllers/LottoController.cs
+++ controllers/LottoController.cs
@@ -8,27 +8,19 @@ namespace api_lotto.controllers
     public class WalletController : ControllerBase
     {
         private readonly LotteryDbContext _context;
-
-        // Constructor: รับ DbContext จาก DI
         public WalletController(LotteryDbContext context)
         {
             _context = context;
         }
 
         
-
-        // -------------------------------
-        // 1. ดูยอดเงินใน Wallet
-        // -------------------------------
         [HttpGet("{memberId}")]
         public IActionResult GetWallet(uint memberId)
         {
-            // หา user จากฐานข้อมูล
             var user = _context.Users.FirstOrDefault(u => u.Uid == memberId);
             if (user == null)
                 return NotFound(new { message = "ไม่พบสมาชิก" });
 
-            // คำนวณยอดเงินจาก WalletTxn
             var balance = _context.WalletTxns
                 .Where(w => w.Uid == memberId && w.Status == true)
                 .Sum(w => (w.TopUp ?? 0) - (w.Withdraw ?? 0));
--- obj/Debug/net9.0/lotto_api.AssemblyInfo.cs
+++ obj/Debug/net9.0/lotto_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ecf2aab1107271cd589fe9215ab2a30f47c6816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b88ef176d2cb048a1487baab6a390f32577457")]
 [assembly: System.Reflection.AssemblyProductAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lotto_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
